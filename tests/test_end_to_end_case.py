e92d2acb72f91530b4c8cda4d4b7a03575560be3
in7hesky <anton.maiko.dev@gmail.com>
2024-12-03 12:31:27 +0300

    make assertions soft

--- tests/test_end_to_end_case.py
+++ tests/test_end_to_end_case.py
@@ -1,33 +1,38 @@
 from tests.abstract.base_test import BaseTest
+from pytest_check import check
 
 SEARCH_TARGET = "Falcon 9"
 
 class TestEndToEndCase(BaseTest):
     
     def test_register_order_with_min_required_data(self, valid_user_data):
+        
         self.home_page.open()
         self.home_page.search_field_input(SEARCH_TARGET)
         
-        assert SEARCH_TARGET in self.search_results_page.get_page_title_string()
-        assert SEARCH_TARGET in self.search_results_page \
-            .get_product_by_index(0).text
+        check.is_in(SEARCH_TARGET, 
+                    self.search_results_page.get_page_title_string())
+        check.is_in(SEARCH_TARGET, 
+                    self.search_results_page.get_product_by_index(0).text)
         
         self.search_results_page.add_product_by_index(0)
         
-        assert self.search_results_page.check_minicart_count_equals(1)
+        check.is_true(self.search_results_page.check_minicart_count_equals(1))
         
         self.search_results_page.navigation.to_cart()
         
-        assert self.cart_page.check_total_cart_items_equals(1)
-        assert self.cart_page.get_cart_item_quantity_by_index(0) == 1
+        check.is_true(self.cart_page.check_total_cart_items_equals(1))
+        check.equal(self.cart_page.get_cart_item_quantity_by_index(0), 1)
         
         self.cart_page.click_checkout_button()
         
-        assert self.checkout_page.check_cart_item_quantity_equals(1)
-        assert self.checkout_page.check_cart_item_by_name_exists(SEARCH_TARGET)
+        check.is_true(self.checkout_page.check_cart_item_quantity_equals(1))
+        check.is_true(self.checkout_page
+                      .check_cart_item_by_name_exists(SEARCH_TARGET))
         
         self.checkout_page.input_required_userdata(valid_user_data)
         self.checkout_page.click_submit_button()
         
-        assert self.order_received_page.check_success_notification_is_present()
+        check.is_true(self.order_received_page
+            .check_success_notification_is_present())
         
